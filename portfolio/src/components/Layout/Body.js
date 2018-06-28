005008e28d1ec9cda85a5281f4e30756665964e3
Unknown <savath.khem@live.com>
2018-06-27 22:46:14 -0400

    REACT

---
+++ portfolio/src/components/Layout/Body.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+const Content = () => (
+
+    <div class="section white">
+    <div class="row container">
+        <h2 class="header">Parallax</h2>
+        <p class="grey-text text-darken-3 lighten-3">Parallax is an effect where the background content or image in this case, is moved at a different speed than
+            the foreground content while scrolling.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, tempora! Ad neque quia, ab adipisci nam laboriosam
+            minus quos accusamus inventore, perferendis vel nulla hic deserunt provident at commodi quis placeat ipsum
+            perspiciatis veniam exercitationem impedit dignissimos. At, quidem? Repudiandae nobis dolorem sunt sequi
+            ipsa quod cumque facere dolor nesciunt optio provident soluta facilis pariatur, similique explicabo, error
+            ad laboriosam veniam, natus repellendus? Laborum, rerum nobis odit cum culpa consequuntur reprehenderit eligendi
+            suscipit doloribus asperiores nulla facere perspiciatis laudantium repellendus ex praesentium beatae libero
+            earum dolores ab harum vitae, quaerat, totam voluptatum? Voluptatem quibusdam, quaerat aliquam porro rem
+            odio eaque molestias. Quam dolorem fuga explicabo minima sint esse omnis, quisquam eligendi nesciunt! Vel
+            placeat illum saepe esse voluptatem dolores reprehenderit atque excepturi laudantium ipsum? Quibusdam possimus
+            ipsa magni odio sunt dolor error expedita vel, assumenda, illum enim ad ut optio numquam iure, esse consequatur
+            quaerat? Ullam temporibus eum aliquid blanditiis fuga, enim quia veniam sit culpa numquam fugiat illum architecto
+            molestias ea necessitatibus inventore! Iste suscipit repellat aliquam ducimus maiores. Praesentium itaque,
+            recusandae enim aperiam necessitatibus nesciunt eos eveniet odio hic dicta et sit culpa molestias natus aut
+            quam! Vitae, officiis. Suscipit eius nostrum quos sed non voluptatum distinctio rem. Laborum cupiditate ducimus
+            omnis, culpa reprehenderit esse? Corporis assumenda nulla magnam consequuntur modi officia laudantium corrupti,
+            quibusdam similique aliquam? Possimus cumque incidunt laboriosam harum libero dicta eligendi, culpa laudantium
+            quis rem itaque magni! Vero, maiores. A iste perspiciatis tenetur possimus ipsa delectus veniam, qui ab reiciendis,
+            tempora vero? Quidem reiciendis incidunt consequuntur molestias provident amet possimus laudantium, maxime
+            eos expedita sed? Beatae a accusantium fugiat, fugit commodi voluptatibus tempora, tenetur unde porro exercitationem
+            perspiciatis! Unde necessitatibus excepturi nobis suscipit iusto tenetur doloribus aliquid ad nesciunt facilis
+            mollitia explicabo quia illum repellat ratione animi repudiandae, eveniet ducimus non nihil nostrum reiciendis
+            et dolorem distinctio. Consequuntur voluptatem fugiat quisquam expedita provident cupiditate, nemo repudiandae
+            modi quaerat facilis ducimus nesciunt culpa, sequi voluptatum explicabo nam earum id libero omnis enim aspernatur
+            nulla asperiores. Harum impedit voluptatibus reprehenderit id ratione aspernatur sequi vero quaerat, accusantium
+            minima animi possimus aliquam commodi ad amet temporibus ex vel! Ipsum perspiciatis at tempore impedit rem
+            dolore totam, dolor quisquam quos eligendi earum molestiae delectus cum, dolores excepturi laborum quia explicabo
+            aperiam eaque ad numquam enim eos. Non, ipsam? Eligendi quae inventore quas beatae iusto odit cupiditate!
+            Rerum, voluptates reprehenderit laudantium ad ipsum fuga, minus earum blanditiis eveniet illum eos! Error
+            illum, voluptatem, saepe, perferendis nulla molestiae atque recusandae earum magni sint inventore! Suscipit
+            dicta quisquam sit obcaecati. Cum iure aliquam aut rem id accusamus impedit. Voluptatum inventore esse quas,
+            corrupti autem aspernatur iste facilis sequi nesciunt rem possimus eveniet deserunt. Modi voluptate provident,
+            consectetur pariatur, ab maxime odio illum at esse dolores tempore unde omnis eum quia aliquam sunt, laboriosam
+            corrupti blanditiis. Fuga nemo accusantium ad repellendus quisquam soluta molestias voluptate, deleniti necessitatibus,
+            qui vel. Optio molestiae voluptatem quasi laboriosam, magni delectus, sed vel tenetur laborum aliquid omnis
+            asperiores libero quaerat pariatur. Nobis quae ratione accusamus corrupti beatae!</p>
+    </div>
+    </div>
+    );
+
+    export default Content;
