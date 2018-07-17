b50334d1c219504e18404280104af715bbcd1591
Unknown <savath.khem@live.com>
2018-07-17 12:58:10 -0400

    portfolio cards

--- src/components/Layout/DrawerLeft/DrawerLeft.js
+++ src/components/Layout/DrawerLeft/DrawerLeft.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import { Drawer, List, IconButton, Divider} from '@material-ui/core/';
-// import PageOptions from './../tileData';
+import { Drawer, List, IconButton, Divider } from '@material-ui/core/';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-// import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import MenuIcon from '@material-ui/icons/Menu';
+import Card from './../Card';
 
 
 const styles = {
@@ -24,6 +23,7 @@ class DrawerLeft extends React.Component {
       [side]: open,
     });
   };
+  
 
   render() {
     const { classes } = this.props;
@@ -33,10 +33,66 @@ class DrawerLeft extends React.Component {
       <IconButton onClick={this.toggleDrawer('left', false)}>
         <ChevronLeftIcon />
       </IconButton>
+      Portfolio
       <Divider />
       <List>
-        testing
-        {/* <PageOptions onChange={this.props.onChange} handleSubmit={this.props.handleSubmit}/> */}
+
+        <Card 
+          title='TrailerPark2.0'
+          src='./images/trailerpark2.0.jpg'
+          url='http://www.trailerpark.site'
+          overview='TrailerPark2.0 is a fullstack application.  This time we used react, express, mongodb, and material-ui. We are able to create a more responsive and dynamic web application.'      
+        />
+
+        <Card 
+          title='ClickyGame'
+          src='./images/clickygame.jpg'
+          url='https://savathkhem.github.io/ClickyGame/'
+          overview='Fun memory game using react'      
+        />
+
+        <Card 
+          title='test'
+          src='https://upload.wikimedia.org/wikipedia/commons/2/23/Lake_mapourika_NZ.jpeg'
+          url='http://www.google.com'
+          overview='blah blah blah'      
+        />
+
+        <Card 
+          title='test'
+          src='https://upload.wikimedia.org/wikipedia/commons/2/23/Lake_mapourika_NZ.jpeg'
+          url='http://www.google.com'
+          overview='blah blah blah'      
+        />
+
+        <Card 
+          title='test'
+          src='https://upload.wikimedia.org/wikipedia/commons/2/23/Lake_mapourika_NZ.jpeg'
+          url='http://www.google.com'
+          overview='blah blah blah'      
+        />
+
+        <Card 
+          title='test'
+          src='https://upload.wikimedia.org/wikipedia/commons/2/23/Lake_mapourika_NZ.jpeg'
+          url='http://www.google.com'
+          overview='blah blah blah'      
+        />
+
+        <Card 
+          title='test'
+          src='https://upload.wikimedia.org/wikipedia/commons/2/23/Lake_mapourika_NZ.jpeg'
+          url='http://www.google.com'
+          overview='blah blah blah'      
+        />
+
+        <Card 
+          title='test'
+          src='https://upload.wikimedia.org/wikipedia/commons/2/23/Lake_mapourika_NZ.jpeg'
+          url='http://www.google.com'
+          overview='blah blah blah'      
+        />
+
       </List>
       </div>
     );
@@ -45,12 +101,7 @@ class DrawerLeft extends React.Component {
       <div>
         <IconButton onClick={this.toggleDrawer('left', true)}><MenuIcon /></IconButton>
         <Drawer open={this.state.left} onClose={this.toggleDrawer('left', false)}>
-          <div
-            // tabIndex={0}
-            // role="button"
-            // onClick={this.toggleDrawer('left', false)}
-            // onKeyDown={this.toggleDrawer('left', false)}
-          >
+          <div>
             {sideList}
           </div>
         </Drawer>
--- src/components/Layout/Head/Head.js
+++ src/components/Layout/Head/Head.js
@@ -1,164 +1,51 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
-import { AppBar, Toolbar, Typography } from '@material-ui/core/';
+import { AppBar, Toolbar, Typography, IconButton } from '@material-ui/core/';
 import Logo from "./../Logo";
+import MenuIcon from '@material-ui/icons/Menu';
 import DrawerLeft from "./../DrawerLeft";
 
-const drawerWidth = 240;
-
 const styles = theme => ({
-  root: {
-    flexGrow: 1,
-  },
-    flex: {
-    flex: 1,
-  },
-  appFrame: {
-    height: 430,
-    zIndex: 1,
-    overflow: 'hidden',
-    position: 'relative',
-    display: 'flex',
-    width: '100%',
-  },
   appBar: {
+    backgroundColor: "#424242",
     zIndex: theme.zIndex.drawer + 1,
-    // position: 'relative',
+    position: 'fixed',
     transition: theme.transitions.create(['margin', 'width'], {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
-  },
-  appBarShift: {
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  'appBarShift-left': {
-    marginLeft: drawerWidth,
-  },
-  'appBarShift-right': {
-    marginRight: drawerWidth,
-  },
-  avatar: {
-    margin: 10,
-  },
-  bigAvatar: {
-    width: 60,
-    height: 60,
-  },
-  avatarButton: {
-    height: 'inherit',
-    marginLeft: 12,
-    marginRight: 20,
-    float: 'right',
-  },
-  menuButton: {
-    marginLeft: 12,
-    marginRight: 20,
-    float: 'right',
-  },
-  hide: {
-    display: 'none',
-  },
-  drawerPaper: {
-    // position: 'relative',
-    width: drawerWidth,
-  },
-  drawerHeader: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: '0 8px',
-    ...theme.mixins.toolbar,
-  },
-  content: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.default,
-    padding: theme.spacing.unit * 3,
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-  },
-  'content-left': {
-    marginLeft: -drawerWidth,
-  },
-  'content-right': {
-    marginRight: -drawerWidth,
-  },
-  contentShift: {
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  'contentShift-left': {
-    marginLeft: 0,
-  },
-  'contentShift-right': {
-    marginRight: 0,
   },
 });
 
 class NavBar extends React.Component {
   state = {
     open: false,
-    anchor: 'left',
-    auth: true,
-    anchorEl: null,
+    drawerOpen: false,
+  };
+
+  handleDrawerClick = () => {
+    this.setState(state => ({ drawerOpen: !state.drawerOpen }));
+    this.setState(state => ({ open: !state.open }));
+
   };
   
-  handleChange = (event, checked) => {
-    this.setState({ auth: checked });
-  };
-
-  handleMenu = event => {
-    this.setState({ anchorEl: event.currentTarget });
-  };
-
-  handleClose = () => {
-    this.setState({ anchorEl: null });
-  };
-
-  handleChangeAnchor = event => {
-    this.setState({
-      anchor: event.target.value,
-    });
-  };
-
-
   render() {
     const { classes } = this.props;
-    const { anchor, open } = this.state;
-    const { auth, anchorEl } = this.state;
-    const openlogin = Boolean(anchorEl);
+    const { open } = this.state;
+
 
     return (
       <div>
         <AppBar
-          position="fixed"
-          className={classNames(classes.appBar, {
-            [classes.appBarShift]: open,
-            [classes[`appBarShift-${anchor}`]]: open,
-          })}
-          style={{backgroundColor: "#424242"}}
+          className={classes.appBar}
         >
           <Toolbar disableGutters={!open}>
 
-            {/* <IconButton
-              color="inherit"
-              aria-label="open drawer"
-              onClick={this.handleDrawerOpen}
-              className={classNames(classes.menuButton, open && classes.hide)}>
-              <MenuIcon />
-            </IconButton> */}
-            <DrawerLeft onChange={this.props.onChange} handleSubmit={this.props.handleSubmit}/>
-
+         {/* <IconButton onClick={this.handleProfileClick}>
+            <MenuIcon />
+          </IconButton> */}
+          <DrawerLeft />
 
             <Typography variant="title" color="inherit" className={classes.flex}>
               <Logo />
